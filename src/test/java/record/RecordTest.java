4cfead9af06aef0aa2db926982193f067c1fa0d9
prateek2378 <prateek2378@gmail.com>
2021-06-18 02:40:32 +0530

    RecordTest File added

--- src/test/java/record/RecordTest.java
+++ src/test/java/record/RecordTest.java
@@ -1,6 +1,5 @@
 package record;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +7,62 @@ import static org.junit.jupiter.api.Assertions.*;
 class RecordTest {
 
     @Test
-    void shouldAddEmployee() {
+    void getNoOfEmployees() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void getDetails() {
+    }
+
+    @Test
+    void getAllDetails() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void editID() {
+    }
+
+    @Test
+    void editName() {
+    }
+
+    @Test
+    void editAddress() {
+    }
+
+    @Test
+    void editContactInformation() {
+    }
+
+    @Test
+    void editDepartment() {
+    }
+
+    @Test
+    void editSalary() {
+    }
+
+    @Test
+    void editDesignation() {
+    }
+
+    @Test
+    void editDateOfJoining() {
+    }
+
+    @Test
+    void editTechnologies() {
+    }
+
+    @Test
+    void editManager() {
     }
 }
